3326265c238fc17e3868cc4ecd6dc155ae306177
Litvin-13 <littlekristina@tut.by>
2026-02-06 17:13:51 +0300

    init

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,6 +7,10 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
+
+
 export class App {
   protected readonly title = signal('el-pixel-hr');
+
+
 }
