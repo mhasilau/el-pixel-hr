00db89c0974bf52f93e3b6b2a6473545257a7103
pashaSok <syffiks1998@gmail.com>
2026-02-07 05:00:21 +0100

    feat: created internshipApplication empty component

---
+++ src/app/InternshipApplicationComponent/InternshipApplication.component.spec.ts
@@ -0,0 +1,27 @@
+/* tslint:disable:no-unused-variable */
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { InternshipApplicationComponent } from './InternshipApplication.component';
+
+describe('InternshipApplicationComponent', () => {
+  let component: InternshipApplicationComponent;
+  let fixture: ComponentFixture<InternshipApplicationComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [InternshipApplicationComponent],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(InternshipApplicationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/InternshipApplicationComponent/InternshipApplication.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-InternshipApplicationComponent',
+  templateUrl: './InternshipApplication.component.html',
+  styleUrls: ['./InternshipApplication.component.scss'],
+})
+export class InternshipApplicationComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
