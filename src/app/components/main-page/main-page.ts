0ea6a32dfde101bad9c91d4c8a45bef40c56b455
Litvin-13 <littlekristina@tut.by>
2026-02-07 17:50:59 +0300

    first-one

---
+++ src/app/components/main-page/main-page.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { Nav } from "../nav/nav";
+
+@Component({
+  selector: 'app-main-page',
+  imports: [MatButtonModule, MatIconModule, Nav],
+  templateUrl: './main-page.html',
+  styleUrl: './main-page.scss',
+})
+export class MainPage {
+
+
+}
